b3e25448e98836ed84b47f1602a6bf440c4e2f17
Raza <77836119+Ahmedlathiya@users.noreply.github.com>
2021-12-19 17:46:52 +0500

    Add files via upload

---
+++ Hackathon App/src/core/config.js
@@ -0,0 +1,10 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAmPRzqrnCipeSePdih-Bs8IqEbpyr5MwM",
+  authDomain: "khana-skl.firebaseapp.com",
+  projectId: "khana-skl",
+  storageBucket: "khana-skl.appspot.com",
+  messagingSenderId: "544002867871",
+  appId: "1:544002867871:web:d533f5865ccf832a2058aa"
+}
+
